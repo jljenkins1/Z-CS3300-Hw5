b9c7cf267efb4a218615eee9fe7a503757978cda
jljcb <jljcb@DellXPS13.dhcp.missouri.edu>
2024-03-01 15:23:23 -0600

    Created main class

---
+++ src/FakeSpeare/main.java
@@ -0,0 +1,13 @@
+package FakeSpeare;
+
+public class main {
+
+	public static void main(String[] args) {
+		// Instantiate a pizzaOrder, perform operations based on the requirements.
+		//PizzaOrder order = new PizzaOrder();
+		
+		// Adds pizzas to the cart, selects cooking strategies for the pizzas in the cart, prints pizza order cart. Calls checkout to calculate the bill, throws exception if triggered.
+		// TODO
+		System.out.println("Hello");
+	}
+}
