d229503e66c09e60e048513e4d4d2f05eeb5b772
jljcb <jljcb@DellXPS13>
2024-03-12 20:26:18 -0500

    added pizza cooking factory method

--- src/fakeSpeare/Main.java
+++ src/fakeSpeare/Main.java
@@ -6,6 +6,8 @@ public class Main {
 		PizzaOrder order = new PizzaOrder();
 		
 		// Adds pizzas to the cart, selects cooking strategies for the pizzas in the cart, prints pizza order cart. Calls checkout to calculate the bill, throws exception if triggered.
-		// TODO
+		 PizzaCookingFactory factory = new PizzaCookingFactory();
+	     AbstractPizza margheritaPizza = factory.createPizza(PizzaType.MARGHERITA);
+	     AbstractPizza hawaiianPizza = factory.createPizza(PizzaType.HAWAIIAN);
 	}
 }
